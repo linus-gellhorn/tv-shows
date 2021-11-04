c5847c12638cce671ee9490e8ae82e95e3181011
Linus Gellhorn <linus.g@hotmail.com>
2021-11-04 17:35:49 +0000

    Created Episode component and mapped through data to display barebones episode info

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
 import episodes from "./episodes.json";
-
-console.log(`Imported ${episodes.length} episode(s)`);
+import Episode from "./Episode";
 
 function App(): JSX.Element {
-  return <h1>TV Shows App</h1>;
+  return (
+    <>
+      <h1>TV Shows App</h1>
+      {episodes.map((episode) => (
+        <Episode
+          key={episode.id}
+          id={episode.id}
+          name={episode.name}
+          season={episode.season}
+          number={episode.number}
+          image={episode.image}
+          summary={episode.summary}
+          type={episode.type}
+          airdate={episode.airdate}
+          airtime={episode.airtime}
+          airstamp={episode.airstamp}
+          runtime={episode.runtime}
+          url={episode.url}
+          _links={episode._links}
+        />
+      ))}
+    </>
+  );
 }
 
 export default App;
---
+++ src/Episode.tsx
@@ -0,0 +1,30 @@
+interface EpisodeProps {
+  id: number;
+  url: string;
+  name: string;
+  season: number;
+  number: number;
+  type: string;
+  airdate: string;
+  airtime: string;
+  airstamp: string;
+  runtime: number;
+  image: {
+    medium: string;
+    original: string;
+  };
+  summary: string;
+  _links: { self: { href: string } };
+}
+
+export default function Episode(props: EpisodeProps): JSX.Element {
+  return (
+    <>
+      <h2>
+        {props.name} - Season: {props.season} Episode: {props.number}
+      </h2>
+      <img src={props.image.medium} alt="" />
+      <p>{props.summary}</p>
+    </>
+  );
+}
