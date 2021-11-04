9a36e878c2ceed23e68b454c9604ecb9f033b926
Linus Gellhorn <linus.g@hotmail.com>
2021-11-04 16:08:54 +0000

    Formatted and linted - now should be done on autosave though

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import episodes from './episodes.json'
+import episodes from "./episodes.json";
+
+console.log(`Imported ${episodes.length} episode(s)`);
 
 function App(): JSX.Element {
   return <h1>TV Shows App</h1>;
