ae4cf75ab894a2e17d62b9154c2cb831bf422c7f
Linus Gellhorn <linus.g@hotmail.com>
2021-11-05 11:50:36 +0000

    Added search bar with two-way input binding

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import "./App.css";
 import EpisodeList from "./components/EpisodeList";
 import MainFooter from "./components/MainFooter";
-import "./App.css";
+import MainHeader from "./components/MainHeader";
 
 function App(): JSX.Element {
   return (
     <>
-      <h1>Game Of Thrones Episodes</h1>
+      <MainHeader />
       <EpisodeList />
       <MainFooter />
     </>
