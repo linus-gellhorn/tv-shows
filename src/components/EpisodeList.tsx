c1d981b01f27cdfde84e5a507a17691fc1c89cc0
Linus Gellhorn <linus.g@hotmail.com>
2021-11-10 14:11:51 +0000

    Added functional episode selector

--- src/components/EpisodeList.tsx
+++ src/components/EpisodeList.tsx
@@ -4,15 +4,33 @@ import { useState } from "react";
 
 export default function EpisodeList(): JSX.Element {
   const [searchTerm, setSearchTerm] = useState("");
+  const [selected, setSelected] = useState("");
 
-  const filteredEpisodes = episodes.filter(
-    (episode) =>
-      episode.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      episode.summary.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const filteredEpisodes =
+    selected !== ""
+      ? episodes.filter((episode) => episode.name === selected)
+      : episodes.filter(
+          (episode) =>
+            episode.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            episode.summary.toLowerCase().includes(searchTerm.toLowerCase())
+        );
 
   return (
     <>
+      <label htmlFor="episodes">Choose a specifc episode: </label>
+      <select name="episode" onChange={(e) => setSelected(e.target.value)}>
+        <option key={0} value="">
+          Choose
+        </option>
+        {episodes.map((episode) => (
+          <option key={episode.id} value={episode.name}>
+            S0{episode.season}E
+            {episode.number > 9 ? episode.number : "0" + episode.number} -{" "}
+            {episode.name}
+          </option>
+        ))}
+      </select>
+      <p>Or search:</p>
       <input
         type="text"
         placeholder="Search for episode(s)"
